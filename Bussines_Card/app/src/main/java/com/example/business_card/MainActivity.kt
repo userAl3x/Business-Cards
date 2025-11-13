ee8752a4605dfca07eddf579227fe126a55d70bb
Alex Jiménez <121909423+userAl3x@users.noreply.github.com>
2025-11-13 17:09:21 +0100

    Update MainActivity.kt
    Añadimos las siguietnes variables:
    - Estados para los TextField
    - Datos de la tarjeta
    - Estados de los Checkbox

--- Bussines_Card/app/src/main/java/com/example/business_card/MainActivity.kt
+++ Bussines_Card/app/src/main/java/com/example/business_card/MainActivity.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.business_card.ui.theme.Business_CardTheme
@@ -29,8 +33,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCard(modifier: Modifier = Modifier) {
-    Text(
-        text = "Business Card",
-        modifier = modifier
-    )
+    // Estados para TextField
+    var inputName by remember { mutableStateOf("") }
+    var inputApellidos by remember { mutableStateOf("") }
+    var inputCargo by remember { mutableStateOf("") }
+    var inputEmpresa by remember { mutableStateOf("") }
+    var inputEmail by remember { mutableStateOf("") }
+    var inputTelefono by remember { mutableStateOf("") }
+
+    // Datos de la tarjeta (lo que se muestra en la Card)
+    var cardName by remember { mutableStateOf("Tu Nombre") }
+    var cardApellidos by remember { mutableStateOf("") }
+    var cardCargo by remember { mutableStateOf("") }
+    var cardEmpresa by remember { mutableStateOf("") }
+    var cardEmail by remember { mutableStateOf("") }
+    var cardTelefono by remember { mutableStateOf("") }
+
+    // Estados de los Checkbox
+    var showApellidos by remember { mutableStateOf(true) }
+    var showCargo by remember { mutableStateOf(true) }
+    var showEmpresa by remember { mutableStateOf(true) }
 }
