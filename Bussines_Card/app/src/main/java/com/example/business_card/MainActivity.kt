a743333d8d064a935a3e316663746598fca857e7
Alex Jiménez <121909423+userAl3x@users.noreply.github.com>
2025-11-13 19:10:48 +0100

    Añadimos la funcion para los iconos de la tarjeta y sus imports

--- Bussines_Card/app/src/main/java/com/example/business_card/MainActivity.kt
+++ Bussines_Card/app/src/main/java/com/example/business_card/MainActivity.kt
@@ -13,6 +13,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -26,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -173,6 +179,18 @@ fun calcularProgreso(
     return completados / total
 }
 
+//funcion para los iconos de la tarjeta
+fun obtenerIcono(nombre: String): ImageVector {
+    return when(nombre) {
+        "Estrella" -> Icons.Default.Star
+        "Casa" -> Icons.Default.Home
+        "Favorito" -> Icons.Default.Favorite
+        "Configuración" -> Icons.Default.Settings
+        else -> Icons.Default.Star
+    }
+}
+
+//funcion para los colores de fondo de la tarjeta
 fun obtenerColorFondo(fondo: Int, colorido: Boolean): Color {
     if (!colorido) {
         // Fondos neutros (grises claros)
